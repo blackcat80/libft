76b9559042379daef815e9c65ac9f6813d5bbb35
Christian Sitja Bayona <csitja-b@cbr10s3.42barcelona.com>
2022-09-21 15:40:46 +0200

    subida funcion itoa

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: csitja-b <csitja-b@student.42barcel>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/16 17:49:18 by csitja-b          #+#    #+#              #
-#    Updated: 2022/09/19 20:25:47 by csitja-b         ###   ########.fr        #
+#    Updated: 2022/09/20 18:37:44 by csitja-b         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,7 +22,8 @@ SRC = ft_isalpha.c ft_isprint.c ft_memset.c ft_bzero.c ft_isascii.c ft_memcpy.c 
 	  ft_strlen.c ft_isalnum.c ft_isdigit.c ft_memmove.c ft_strlcpy.c ft_strlcat.c \
 	  ft_toupper.c ft_tolower.c ft_strchr.c ft_strrchr.c ft_memchr.c ft_memcmp.c \
 	  ft_strncmp.c ft_strnstr.c ft_atoi.c ft_calloc.c ft_strdup.c ft_putchar_fd.c \
-	  ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c ft_striteri.c ft_strmapi.c
+	  ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c ft_striteri.c ft_strmapi.c \
+	  ft_itoa.c
 
 OBJ = $(SRC:.c=.o)
 
---
+++ ft_itoa.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csitja-b <csitja-b@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 15:13:21 by csitja-b          #+#    #+#             */
+/*   Updated: 2022/09/20 18:37:48 by csitja-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	long	nbr;
+	size_t	size;
+
+	nbr = n;
+	size = n > 0 ? 0 : 1;
+	nbr = nbr > 0 ? nbr : -nbr;
+	while (n)
+	{
+		n /= 10;
+		size++;
+	}
+	if (!(str = (char *)malloc(size + 1)))
+		return (0);
+	*(str + size--) = '\0';
+	while (nbr > 0)
+	{
+		*(str + size--) = nbr % 10 + '0';
+		nbr /= 10;
+	}
+	if (size == 0 && str[1] == '\0')
+		*(str + size) = '0';
+	else if (size == 0 && str[1] != '\0')
+		*(str + size) = '-';
+	return (str);
+}
